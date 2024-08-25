3a0b2f26a8f10f3016d15c862e8bac569da226b5
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-08-25 19:35:24 +0530

    Added transparent logo with accent color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Logo from '/Logo/CyanLogo.png'
+import Logo1 from '/Logo/ColorLogoTrans.png'
 const Navbar = () => {
   return (
     <div className='w-[80%] mx-auto flex justify-between items-center'>
       <div className='max-h-full'>
-        <img src={Logo} className='h-[4rem] '></img>
+        <img src={Logo1} className='h-[4rem] '></img>
       </div>
 
       <ul>
-        <li>yo</li>
+        <li className='font-medium text-2xl text-AccentCoral'>Home</li>
       </ul>
     </div>
   )
