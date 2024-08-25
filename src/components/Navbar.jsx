0db634e46bbb3e447995912e77c80b8a2305ed2f
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-08-25 20:06:08 +0530

    Made the nav bar sticky and color changing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Logo from '/Logo/CyanLogo.png'
 import Logo1 from '/Logo/ColorLogoTrans.png'
 const Navbar = () => {
-  return (
-    <div className='w-[80%] mx-auto flex justify-between items-center'>
-      <div className='max-h-full'>
-        <img src={Logo1} className='h-[4rem] '></img>
-      </div>
+  const [sticky, setSticky] = useState(false);
 
-      <ul>
-        <li className='font-medium text-2xl text-AccentCoral'>Home</li>
-      </ul>
+  useEffect( () => {
+    window.addEventListener('scroll', () => {
+        (window.scroll > 100)? setSticky(true) : setSticky(false);
+    })
+  }, []);
+
+  return (
+    <div className='w-full fixed top-0'>
+        <div className={`w-[80%] mx-auto flex justify-between items-center ${sticky? "bg-PrimaryBlue" : "bg-inherit"} `}>
+        <div className='max-h-full'>
+            <img src={Logo1} className='h-[4rem] '></img>
+        </div>
+
+        <ul className='font-medium text-2xl text-AccentCoral flex '>
+            <li className='mx-5'>Home</li>
+            <li className='mx-5'>Search</li>
+            <li className='mx-5'>Visualize</li>
+            <li className='mx-5'>Upload</li>
+        </ul>
+        </div>
     </div>
   )
 }
