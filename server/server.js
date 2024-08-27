c366b3c40053452dfadefb9f42513a86651dbece
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-08-27 17:05:44 +0530

    Restricted the sites that can pass requests with cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors') ;
 
+const corsOptions = {
+    origin : ["http://localhost:5173"],
+}
+
+app.use(cors(corsOptions));
+
 app.get('/api', (req, res) => {
     res.json({fruits : ["apple", "orange", "banana"]});
 })
