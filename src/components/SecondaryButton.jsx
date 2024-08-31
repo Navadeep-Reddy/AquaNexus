9f801b8ea1f8ba42401bde22170681c69210f663
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-08-31 14:02:09 +0530

    Added animations to hero section and secondary button

--- src/components/SecondaryButton.jsx
+++ src/components/SecondaryButton.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const SecondaryButton = ({children}) => {
   return (
-      <button className='border-none w-[160px] h-[40px] rounded-md bg-PrimaryBlue font-semibold text-xl text-BackgroundSand shadow-md' type='submit'>{children}</button>
+    
+      <motion.button whileHover = {{scale: 1.05}} 
+      whileTap = {{scale: 0.9, rotate: "10deg"}}
+      className='border-none w-[160px] h-[40px] rounded-md bg-PrimaryBlue font-semibold text-xl text-BackgroundSand shadow-md' type='submit'
+       >
+        {children}
+      
+      </motion.button>
   )
 }
 
