44f773ed77518caf1da1705f2241c3f54331bff2
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-09-05 13:19:13 +0530

    Created UI for ML prediction

---
+++ src/components/ChartComponents/MyBarChart.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
+
+const MyBarChart = ({data}) => {
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <BarChart
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+        <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default MyBarChart;
